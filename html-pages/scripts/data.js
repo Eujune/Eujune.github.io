72f9ece7052fb4bf36950c6ae26a6bbc9c7c8502
accelotron <saferjune0592@gmail.com>
2021-04-23 21:02:09 +0300

    font size and deleted stuff

--- html-pages/scripts/data.js
+++
@@ -1,6 +0,0 @@
-data = ["9а класс 591 школы", "Сюда всё равно никто не заходит", "9а класс"]
-
-document.getElementsByClassName('header-title')[0].innerHTML=data[0]
-document.getElementsByClassName('header-subtitle')[0].innerHTML=data[1]
-document.getElementsByClassName('footer-author')[0].innerHTML=data[0]
-document.getElementsByTagName('title')[0].innerHTML=data[2]
